dcf536ee2ebcbd82eb43f538f03d679f51c0363d
csdpt <vaibhaviarolkar07@gmail.com>
2022-11-30 11:10:06 +0530

    afgka

--- ftester.java
+++ ftester.java
@@ -2,8 +2,14 @@ public class ftester
 {
     public static void main (String[] args)
     {
-        chair c =new chair();
+        chair c =new chair();vxbzb
         c.setprice(789);
         c.display();
+        64104
+        410714
+        130717
+        13071
+        14071
+        13071
     }
 }
